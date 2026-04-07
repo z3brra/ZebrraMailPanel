d4cbe23080173e896a2304cc3b6ad5bb6caf600a
z3brra <pas.tp2000@gmail.com>
2026-04-07 21:37:46 +0200

    Fix : Fix Docker build error

--- zebrra_mail_panel/src/pages/token/TokensPage.tsx
+++ zebrra_mail_panel/src/pages/token/TokensPage.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Plus } from "lucide-react";
+// import { Plus } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { useDebouncedValue } from "@/hooks/useDebouncedValue";
 
 import type { TokenListQuery } from "@/features/tokens/types/token.types";
